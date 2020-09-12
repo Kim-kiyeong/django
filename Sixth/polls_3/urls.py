dd9de71eafb6bac943eacbbd13c5ca1fcefc5944
rldud <kiyoung3431@gmail.com>
2020-09-13 03:49:28 +0900

    6th Django Practice - form ok

--- Sixth/polls_3/urls.py
+++ Sixth/polls_3/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('polls_3/', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('polls_3/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('polls_3/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('polls_3/<int:pk>/vote/', views.vote, name='vote'),
